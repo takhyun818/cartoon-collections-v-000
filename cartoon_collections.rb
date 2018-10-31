f2aa0a2438c73561298477390a4ff02bce9d554a
Tak H Kim <takhyun818@gmail.com>
2018-10-31 04:58:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,9 +14,9 @@ def summon_captain_planet(capt)
   no_capt
 end
 
-def long_planeteer_calls(array)
+def long_planeteer_calls(planteteer)
   i = 0
-  if  array.any? {|i| i.length > 4}
+  if  planteteer.any? {|i| i.length > 4}
     return true
   else
     return false
@@ -25,9 +25,9 @@ def long_planeteer_calls(array)
 end
 
 
-def find_the_cheese(array)
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    array.find do |type|
+    cheese.find do |type|
     cheese_types.include?(type)
   end
 end
